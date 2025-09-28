0d91b4bfb157c46173d91ab5744cd671179fa81f
ManashSwain <manash0802@gmail.com>
2025-09-28 12:40:49 +0530

    feat: implement new route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import dotenv from'dotenv';
 const app = express();
 const port = 5000 || process.env.PORT ;
+dotenv.config()
 
 app.get('/' , (req,res)=>{
     res.send("Hello world");
+})
+
+app.get('/details', (req,res)=>{
+    res.json({
+        name: "manash",
+        age: 15
+    })
 })
 
 app.listen(port, ()=>{
